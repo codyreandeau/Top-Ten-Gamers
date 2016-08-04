59d4ce6474a26e37f8553c7ca8b4f82bbbb2adac
Cody Reandeau <codyreandeau@yahoo.com>
2016-08-03 21:43:48 -0400

    Created print method

--- LinkedList.java
+++ LinkedList.java
@@ -3,6 +3,10 @@ public class LinkedList {
   private Node head;
   private Node tail;
   
+  
+  /**
+   * Method that checks the size of the list
+   */
   public int size() {
     int counter = 0;
     Node temp = head;
@@ -13,9 +17,23 @@ public class LinkedList {
     return counter;
  } 
   
+  /**
+   * Method that checks if the head is Empty
+   */
   public boolean isEmpty() {
     return head == null;
   }
   
+  /**
+   * Method that prints out each node in the list
+   */
+ public void print() {
+    Node temp = head;
+    while(temp!= null)
+    {
+      System.out.println(temp.getName());
+      temp = temp.getNext();
+    }
+  }
   
 }
